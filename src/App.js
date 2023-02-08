baabfc944315f7ca0829606e4f471260bea8d564
Kamil Otocki <kamillooto@gmail.com>
2023-02-08 16:03:39 +0100

    Add commponents (Layout.js and Home.js)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import api from './api/axiosConfig';
 import { useState, useEffect } from 'react';
+import Layout from './commponents/Layout';
+import { Routes, Route } from 'react-router-dom';
+import Home from './commponents/home/Home';
 
 function App() {
 
@@ -10,13 +13,8 @@ function App() {
 
     try
     {
-
       const response = await api.get("/api/v1/movies");
-      
-      console.log(response.data);
-
       setMovies(response.data);
-
     } catch(err)
     {
       console.log(err);
@@ -27,11 +25,17 @@ function App() {
 
   useEffect(() =>{
     getMovies();
-  },[])
+  }, [])
 
   return (
     <div className="App">
       
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<Home/>}></Route>
+        </Route>
+      </Routes>
+
     </div>
   );
 }
