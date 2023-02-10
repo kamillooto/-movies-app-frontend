5e2e58a33e2a59ee10f399de35c5f9f97b62fe6d
Kamil Otocki <kamillooto@gmail.com>
2023-02-10 15:33:18 +0100

    Add Header and change Hero.css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import Layout from './commponents/Layout';
 import { Routes, Route } from 'react-router-dom';
 import Home from './commponents/home/Home';
+import Header from './commponents/header/Header';
 
 function App() {
 
@@ -30,6 +31,7 @@ function App() {
   return (
     <div className="App">
       
+      <Header/>
       <Routes>
         <Route path="/" element={<Layout/>}>
         <Route path="/" element={<Home movies={movies} />} ></Route>
