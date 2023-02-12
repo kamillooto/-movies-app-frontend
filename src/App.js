24aeda34087d22a0c7cf22d01e8cde9481c975c6
Kamil Otocki <kamillooto@gmail.com>
2023-02-12 15:56:22 +0100

    update Review functionality

--- src/App.js
+++ src/App.js
@@ -12,56 +12,59 @@ function App() {
 
   const [movies, setMovies] = useState();
   const [movie, setMovie] = useState();
-  const [reviews, setReviews] = useState();
+  const [reviews, setReviews] = useState([]);
 
   const getMovies = async () =>{
-
+    
     try
     {
 
       const response = await api.get("/api/v1/movies");
 
       setMovies(response.data);
-      
-    } catch(err)
+
+    } 
+    catch(err)
     {
       console.log(err);
     }
   }
 
   const getMovieData = async (movieId) => {
-
-    try
+     
+    try 
     {
-      const response = await api.get(`/api/v1/movies/${movieId}`);
+        const response = await api.get(`/api/v1/movies/${movieId}`);
 
-      const singleMovie = response.data;
+        const singleMovie = response.data;
 
-      setMovies(singleMovie);
+        setMovie(singleMovie);
 
-      setReviews(singleMovie.reviews)
-    }
-    catch(error)
+        setReviews(singleMovie.reviewIds);
+        
+
+    } 
+    catch (error) 
     {
       console.error(error);
     }
 
   }
 
-  useEffect(() =>{
+  useEffect(() => {
     getMovies();
-  }, [])
+  },[])
 
   return (
     <div className="App">
-      
       <Header/>
       <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route path="/" element={<Home movies={movies} />} ></Route>
-          <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
-          <Route path="/Reviews/:movieId" element={<Reviews getMovieData ={getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
-        </Route>
+          <Route path="/" element={<Layout/>}>
+            <Route path="/" element={<Home movies={movies} />} ></Route>
+            <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+            <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
+            {/* <Route path="*" element = {<NotFound/>}></Route> */}
+          </Route>
       </Routes>
 
     </div>
