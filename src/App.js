a9a9b04a68676e52390339610bd4538ea120d52e
Kamil Otocki <kamillooto@gmail.com>
2023-02-12 16:14:47 +0100

    add NotFound page and change styles

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './commponents/home/Home';
 import Header from './commponents/header/Header';
 import Trailer from './commponents/trailer/Trailer';
 import Reviews from './commponents/reviews/Reviews';
+import NotFound from './commponents/notFound/NotFound';
 
 function App() {
 
@@ -63,7 +64,7 @@ function App() {
             <Route path="/" element={<Home movies={movies} />} ></Route>
             <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
             <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
-            {/* <Route path="*" element = {<NotFound/>}></Route> */}
+            <Route path="*" element = {<NotFound/>}></Route>
           </Route>
       </Routes>
 
